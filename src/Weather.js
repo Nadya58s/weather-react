728b6c706e20b280fe31f1d28da15808b38c7af4
Nadia Sylvester <126354130+Nadya58s@users.noreply.github.com>
2023-06-25 12:21:39 +0100

    axios integration to show city weather

---
+++ src/Weather.js
@@ -0,0 +1,29 @@
+/** @format */
+
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${Math.round(response.data.main.temp)}°C`
+    );
+  }
+
+  let apiKey = "976d233bc82d7027f8f478bab9ea4906";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+   <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+  );
+}
